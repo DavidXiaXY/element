cbf124d659305214c4f300ace655902eccd034db
DavidXia <13818094497@163.com>
2018-12-12 16:50:12 +0800

    第一次更新项目

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+	<div id="app">
+
+		<header></header>
+		<aside>
+			<admin-aside></admin-aside>
+		</aside>
+		<main>
+			<router-view/>
+		</main>
+	</div>
+</template>
+<script type="text/javascript">
+	import AdminAside from "@/components/Aside"
+	
+	export default {
+		data() {
+			return {};
+		},
+		methods: {},
+		components:{
+			AdminAside
+		}
+	}
+</script>
+<style>
+	html,
+	body,
+	#app {
+		margin: 0;
+		height: 100%;
+		width: 100%;
+		background-color: #f2f2f2;
+	}
+	
+	header {
+		height: 50px;
+		width: 100%;
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		background-color: #fff;
+		border-bottom: solid 1px #ccc;
+	}
+	
+	aside {
+		height: 100%;
+		width: 220px;
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		background-color: #20222A;
+		color: #fff;
+		
+	}
+	
+	main {
+		padding: 15px;
+		box-sizing: border-box;
+		position: fixed;
+		top: 50px;
+		left: 220px;
+		right: 0px;
+		bottom: 0px;
+		overflow: hidden;
+		overflow-y: auto;
+	}
+</style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,171 @@
+<template>
+
+	<div class="layout">
+
+		<h1>隐藏类</h1>
+
+		<div class="hidden-xs-only">hidden-xs-only - 当视口在 xs 尺寸时隐藏</div>
+		<div class="hidden-sm-only">hidden-sm-only - 当视口在 sm 尺寸时隐藏</div>
+		<div class="hidden-sm-and-down">hidden-sm-and-down - 当视口在 sm 及以下尺寸时隐藏</div>
+		<div class="hidden-sm-and-up ">hidden-sm-and-up - 当视口在 sm 及以上尺寸时隐藏</div>
+		<div class="hidden-md-only">hidden-md-only - 当视口在 md 尺寸时隐藏</div>
+		<div class="hidden-md-and-down">hidden-md-and-down - 当视口在 md 及以下尺寸时隐藏</div>
+		<div class="hidden-md-and-up">hidden-md-and-up - 当视口在 md 及以上尺寸时隐藏</div>
+		<div class="hidden-lg-only">hidden-lg-only - 当视口在 lg 尺寸时隐藏</div>
+		<div class="hidden-lg-and-down">hidden-lg-and-down - 当视口在 lg 及以下尺寸时隐藏</div>
+		<div class="hidden-lg-and-up">hidden-lg-and-up - 当视口在 lg 及以上尺寸时隐藏</div>
+		<div class="hidden-xl-only">hidden-xl-only - 当视口在 xl 尺寸时隐藏</div>
+ 
+
+
+
+		<h1>基础布局-24格子</h1>
+		
+		
+		<el-row :gutter="20" tag="ul">
+			<el-col :span="6" :offset="6" tag="li">
+				<div class="grid-content bg-purple" style="background-color: red;"></div>
+			</el-col>
+			<el-col :span="6" :offset="6" tag="li">
+				<div class="grid-content bg-purple" style="background-color: blue;"></div>
+			</el-col>
+		</el-row>
+		
+		<el-row :gutter="20">
+			<el-col :span="6" :offset="6">
+				<div class="grid-content bg-purple" style="background-color: red;"></div>
+			</el-col>
+			<el-col :span="6" :offset="6">
+				<div class="grid-content bg-purple" style="background-color: blue;"></div>
+			</el-col>
+		</el-row>
+		<el-row>
+			<el-col :span="24">
+				<div class="grid-content bg-purple-dark"></div>
+			</el-col>
+		</el-row>
+		<el-row>
+			<el-col :span="12">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="12">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+		</el-row>
+		<el-row :gutter="20">
+			<el-col :span="8">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="8">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+			<el-col :span="8">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+		</el-row>
+		<el-row>
+			<el-col :span="6">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="6">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+			<el-col :span="6">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="6">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+		</el-row>
+		<el-row>
+			<el-col :span="4">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="4">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+			<el-col :span="4">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="4">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+			<el-col :span="4">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="4">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+			<el-col :span="4">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+		</el-row>
+		
+		<el-row>
+			<el-col :span="2">
+				<div class="grid-content bg-purple" style="background-color: yellow;">123123123</div>
+			</el-col>
+			<el-col :span="2">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+			<el-col :span="2">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="2">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+			<el-col :span="2">
+				<div class="grid-content bg-purple"></div>
+			</el-col>
+			<el-col :span="2">
+				<div class="grid-content bg-purple-light"></div>
+			</el-col>
+
+		</el-row>
+		
+		
+	</div>
+
+</template>
+
+<script>
+	import 'element-ui/lib/theme-chalk/display.css';
+	export default{
+	}
+	
+</script>
+
+<style>
+	.el-row {
+		margin-bottom: 20px;
+		&:last-child {
+			margin-bottom: 0;
+		}
+	}
+	
+	.el-col {
+		border-radius: 4px;
+	}
+	
+	.bg-purple-dark {
+		background: #99a9bf;
+	}
+	
+	.bg-purple {
+		background: #d3dce6;
+	}
+	
+	.bg-purple-light {
+		background: #e5e9f2;
+	}
+	
+	.grid-content {
+		border-radius: 4px;
+		min-height: 36px;
+	}
+	
+	.row-bg {
+		padding: 10px 0;
+		background-color: #f9fafc;
+	}
+</style>
